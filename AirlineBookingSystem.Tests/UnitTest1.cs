6fbb6e50b037994b8c71b73e587793568c90a1f9
Stanislav Chalyi <stas.chalyi@gmail.com>
2024-07-19 17:20:44 +0200

    Initalize project

---
+++ AirlineBookingSystem.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AirlineBookingSystem.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
